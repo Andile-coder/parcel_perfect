fe62eebb6d0e74359e75a9b681f6b98220870a6a
uxshahil <shahil@themidastouch.co.za>
2021-12-13 05:25:50 +0200

    v0.3

--- functions/src/fetchDestination.js
+++ functions/src/fetchDestination.js
@@ -6,21 +6,23 @@ const fetchDestination = async (order, token) => {
     const city = order.billing.city;
 
     const result = await getPlacesByPostcodeAsync(postcode, token);
+    logger.log('info', result);
+
     if (result.errorcode) {
         logger.error("fetchDestination", result.errormessage);
         throw Error;
     } else {
-        const places = result.results;                
+        const places = result.results;
         const filteredPlaces = places.filter(x => x.town.toLowerCase().includes(city.toLowerCase()));
-        
+
         if (filteredPlaces.length) {
             const destination = filteredPlaces[0];
             return destination;
         } else {
+            logger.log('info', 'Places returned by getPlacesByPostcodeAsync does not match user input city');
             return null;
         }
     }
 }
 
 module.exports = fetchDestination;
-
