457fe7282831a87a15484d84d490d448abdf10f6
uxshahil <shahil@themidastouch.co.za>
2022-01-06 19:33:42 +0200

    v0.5

--- functions/src/fetchDestination.js
+++ functions/src/fetchDestination.js
@@ -1,28 +1,30 @@
-const { getPlacesByPostcodeAsync } = require("@themidastouch/parcelperfect/src/places");
-const { getPlacesByNameAsync } = require("@themidastouch/parcelperfect/src/places");
+const { getPlacesByNameAsync, getPlacesByPostcodeAsync } = require("@themidastouch/parcelperfect/src/places");
+
 const logger = require("./logger");
 
 const fetchDestination = async (order, token) => {
     const postcode = order.billing.postcode;
     const city = order.billing.city;
 
-    // const result = await getPlacesByPostcodeAsync(postcode, token);
-    const result = await getPlacesByNameAsync(city, token);
+    const result1 = await getPlacesByNameAsync(city, token);
+    logger.log('info', result1);
 
-    logger.log('info', result);
+    const result2 = await getPlacesByPostcodeAsync(postcode, token);
+    logger.log('info', result2);
+
+    const result = result1 ? result1 : result2;
 
     if (result.errorcode) {
         logger.error("fetchDestination", result.errormessage);
-        throw Error;
+        throw Error;      
     } else {
-        const places = result.results;
+        const places = result1 ? result1.results : result2.results ;
         const filteredPlaces = places.filter(x => x.town.toLowerCase().includes(city.toLowerCase()));
 
         if (filteredPlaces.length) {
             const destination = filteredPlaces[0];
             return destination;
         } else {
-            // logger.log('info', 'Places returned by getPlacesByPostcodeAsync does not match user input city');
             logger.log('info', 'Places returned by getPlacesByNameAsync does not match user input city');
             return null;
         }
@@ -30,3 +32,4 @@ const fetchDestination = async (order, token) => {
 }
 
 module.exports = fetchDestination;
+
